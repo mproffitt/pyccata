5a4bf437cc398fab424e67d10f03587d74caea2a
Martin Proffitt <martin.proffitt@hpe.com>
2018-04-06 09:28:35 +0200

    Versioned to 0.1.9

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages
 
 setup(
     name='pycatta',
-    version='0.1.8',
+    version='0.1.9',
     description='Collation and correlation of data',
     author='Martin Proffitt',
     author_email='mproffitt@jitsc.co.uk',
